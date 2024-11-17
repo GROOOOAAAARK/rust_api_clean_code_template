e02983baa90d4fb36e8aceddfa8a6db997c2d82d
GROOOOAAAARK <t01_n10@protonmail.com>
2024-11-17 22:19:32 +0100

    ✨ implement api infrastructure layer
    creating the web app, gathering routers and making the whole app available to the higher layer

---
+++ src/infrastructure/api.rs
@@ -0,0 +1,19 @@
+extern crate actix_web;
+
+use actix_web::{App, HttpServer}
+
+use crate::infrastructure::ping_api;
+
+fn module_routes() -> Vec<fn> {
+    const routes: Vec<fn> = Vec::new();
+    routes.extend(ping_api::get_routers());
+}
+
+pub fn get_app() -> App {
+    const app = App::new();
+    for route in module_routes() {
+        app.service(route);
+    }
+    app
+}
+
