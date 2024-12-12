4793782b84d23b5a4df88bafc099f62d9baa055e
GROOOOAAAARK <t01_n10@protonmail.com>
2024-12-12 01:06:55 +0100

    ✨ implement create certified information api and declare the main route

--- src/infrastructure/api.rs
+++ src/infrastructure/api.rs
@@ -3,7 +3,9 @@ extern crate actix_web;
 use actix_web::web;
 
 use crate::infrastructure::ping_apis;
+use crate::infrastructure::certified_information_apis;
 
 pub fn routes(service_config: &mut web::ServiceConfig) {
     service_config.service(web::scope("/ping").configure(ping_apis::routes));
+    service_config.service(web::scope("/certified-information").configure(certified_information_apis::routes));
 }
---
+++ src/infrastructure/certified_information_apis.rs
@@ -0,0 +1,28 @@
+extern crate actix_web;
+
+use actix_web::{web::Json, web::ServiceConfig, HttpResponse, post};
+use crate::adapters::create_certified_information_controller::CreateCertifiedInformationController;
+use crate::adapters::http_presenter::HttpPresenter;
+use crate::adapters::ports::traits::AdapterTrait;
+use crate::usecases::create_certified_information::{CreateCertifiedInformationUsecase, CreateCertifiedInformationInput};
+
+// ** Config **
+
+pub fn routes(service_config: &mut ServiceConfig) {
+    service_config.service(create_certified_information);
+}
+
+// ** Routes **
+
+#[post("/certified-information")]
+async fn create_certified_information(
+    request: Json<CreateCertifiedInformationInput>
+) -> HttpResponse {
+    let adapter = CreateCertifiedInformationController::new(
+        CreateCertifiedInformationUsecase::new(),
+        HttpPresenter::new(),
+    );
+    let input: CreateCertifiedInformationInput = request.into_inner();
+    let response = adapter.invoke(input);
+    response
+}
